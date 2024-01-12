2edda73fa3ae33cc02641a94396418dacde49555
joshua constine <joshua.constine97@gmail.com>
2024-01-12 07:09:16 -0800

    added alcohol JSON GET route

--- api/routes.go
+++ api/routes.go
@@ -2,34 +2,37 @@ package api
 
 import (
 	"database/sql"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // InitRoutes initializes the routes for the API.
 func InitRoutes(r *mux.Router, db *sql.DB) {
 	// Define the routes.
 
+	r.HandleFunc("/rentals", func(w http.ResponseWriter, r *http.Request) {
+		GetRentals(w, r, db)
+	}).Methods("GET")
 
+	r.HandleFunc("/rentals/{id}", func(w http.ResponseWriter, r *http.Request) {
+		GetRental(w, r, db)
+	}).Methods("GET")
 
-r.HandleFunc("/rentals",  func(w http.ResponseWriter, r *http.Request) {
-    GetRentals(w, r, db )
-}).Methods("GET") 
+	r.HandleFunc("/rentals/{id}/settings", func(w http.ResponseWriter, r *http.Request) {
+		GetSettingsForRental(w, r, db)
+	}).Methods("GET")
+	r.HandleFunc("/rentals/{id}/timeblocks", func(w http.ResponseWriter, r *http.Request) {
+		GetRentalTimeblocks(w, r, db)
+	}).Methods("GET")
 
-r.HandleFunc("/rentals/{id}",  func(w http.ResponseWriter, r *http.Request) {
-	GetRental(w, r, db )
-}).Methods("GET")
-	
-r.HandleFunc("/rentals/{id}/settings",  func(w http.ResponseWriter, r *http.Request) {
-	GetSettingsForRental(w, r, db )
-}).Methods("GET")
-r.HandleFunc("/rentals/{id}/timeblocks",  func(w http.ResponseWriter, r *http.Request) {
-	GetRentalTimeblocks(w, r, db )
-}).Methods("GET")
+	r.HandleFunc("/rentals/{id}/timeblocks", func(w http.ResponseWriter, r *http.Request) {
+		CreateRentalTimeblock(w, r, db)
+	}).Methods("POST")
 
-r.HandleFunc("/rentals/{id}/timeblocks",  func(w http.ResponseWriter, r *http.Request) {
-	CreateRentalTimeblock(w, r, db )
-}).Methods("POST")
+	r.HandleFunc("/alcohol", func(w http.ResponseWriter, r *http.Request) {
+		GetAlcohol(w, r, db)
 
+	}).Methods("GET")
 
 }
