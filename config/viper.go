d075fb7ea65002c308c637022121e841c612e0b1
joshua constine <joshua.constine97@gmail.com>
2024-02-20 22:19:17 -0800

    modified viper

--- config/viper.go
+++ config/viper.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"errors"
 	"fmt"
 	"os"
 
@@ -19,62 +18,77 @@ type EnvVars struct {
 }
 
 func LoadConfig() (config EnvVars, err error) {
-	viper.AddConfigPath(".")
+	// viper.AddConfigPath(".")
 	// viper.SetConfigName("app")
-	viper.SetConfigType("env")
+	// viper.SetConfigType("env")
+
+	// viper.AutomaticEnv()
+
+	// err = viper.ReadInConfig()
+	// if err != nil {
+	// 	// Fallback to environment variables if config file is not found
+	// 	config.DSN = os.Getenv("DSN")
+	// 	config.PORT = os.Getenv("PORT")
+	// 	config.OBJECT_STORAGE_URL = os.Getenv("OBJECT_STORAGE_URL")
+	// 	config.OBJECT_STORAGE_BUCKET = os.Getenv("OBJECT_STORAGE_BUCKET")
+	// 	config.OBJECT_STORAGE_ACCESS_KEY = os.Getenv("OBJECT_STORAGE_ACCESS_KEY")
+	// 	config.OBJECT_STORAGE_SECRET = os.Getenv("OBJECT_STORAGE_SECRET")
+
+	// 	if config.DSN == "" || config.PORT == "" || config.OBJECT_STORAGE_URL == "" || config.OBJECT_STORAGE_ACCESS_KEY == "" || config.OBJECT_STORAGE_SECRET == "" || config.OBJECT_STORAGE_BUCKET == "" || config.SEND_GRID_KEY == "" {
+	// 		return config, fmt.Errorf("error loading config, %v", err)
+	// 	}
+	// 	return config, nil
+	// }
 
 	viper.AutomaticEnv()
 
-	err = viper.ReadInConfig()
-	if err != nil {
-		// Fallback to environment variables if config file is not found
-		config.DSN = os.Getenv("DSN")
-		config.PORT = os.Getenv("PORT")
-		config.OBJECT_STORAGE_URL = os.Getenv("OBJECT_STORAGE_URL")
-		config.OBJECT_STORAGE_BUCKET = os.Getenv("OBJECT_STORAGE_BUCKET")
-		config.OBJECT_STORAGE_ACCESS_KEY = os.Getenv("OBJECT_STORAGE_ACCESS_KEY")
-		config.OBJECT_STORAGE_SECRET = os.Getenv("OBJECT_STORAGE_SECRET")
+	// Fallback to environment variables if config file is not found
+	config.DSN = os.Getenv("DSN")
+	config.PORT = os.Getenv("PORT")
+	config.OBJECT_STORAGE_URL = os.Getenv("OBJECT_STORAGE_URL")
+	config.OBJECT_STORAGE_BUCKET = os.Getenv("OBJECT_STORAGE_BUCKET")
+	config.OBJECT_STORAGE_ACCESS_KEY = os.Getenv("OBJECT_STORAGE_ACCESS_KEY")
+	config.OBJECT_STORAGE_SECRET = os.Getenv("OBJECT_STORAGE_SECRET")
 
-		if config.DSN == "" || config.PORT == "" || config.OBJECT_STORAGE_URL == "" || config.OBJECT_STORAGE_ACCESS_KEY == "" || config.OBJECT_STORAGE_SECRET == "" || config.OBJECT_STORAGE_BUCKET == "" || config.SEND_GRID_KEY == "" {
-			return config, fmt.Errorf("error loading config, %v", err)
-		}
-		return config, nil
+	if config.DSN == "" || config.PORT == "" || config.OBJECT_STORAGE_URL == "" || config.OBJECT_STORAGE_ACCESS_KEY == "" || config.OBJECT_STORAGE_SECRET == "" || config.OBJECT_STORAGE_BUCKET == "" || config.SEND_GRID_KEY == "" {
+		return config, fmt.Errorf("error loading config, %v", err)
 	}
+	return config, nil
 
-	err = viper.Unmarshal(&config)
+	// err := viper.Unmarshal(&config)
 
-	// validate config here
+	// // validate config here
 
-	if config.DSN == "" {
-		err = errors.New("DSN is required")
-		return
-	}
+	// if config.DSN == "" {
+	// 	err = errors.New("DSN is required")
+	// 	return
+	// }
 
-	if config.PORT == "" {
-		err = errors.New("PORT is required")
+	// if config.PORT == "" {
+	// 	err = errors.New("PORT is required")
 
-	}
+	// }
 
-	if config.OBJECT_STORAGE_URL == "" {
-		err = errors.New("OBJECT_STORAGE_URL is required")
+	// if config.OBJECT_STORAGE_URL == "" {
+	// 	err = errors.New("OBJECT_STORAGE_URL is required")
 
-	}
+	// }
 
-	if config.OBJECT_STORAGE_ACCESS_KEY == "" {
-		err = errors.New("OBJECT_STORAGE_ACCESS_KEY is required")
-	}
+	// if config.OBJECT_STORAGE_ACCESS_KEY == "" {
+	// 	err = errors.New("OBJECT_STORAGE_ACCESS_KEY is required")
+	// }
 
-	if config.OBJECT_STORAGE_SECRET == "" {
-		err = errors.New("OBJECT_STORAGE_SECRET is required")
-	}
+	// if config.OBJECT_STORAGE_SECRET == "" {
+	// 	err = errors.New("OBJECT_STORAGE_SECRET is required")
+	// }
 
-	if config.OBJECT_STORAGE_BUCKET == "" {
-		err = errors.New("OBJECT_STORAGE_BUCKET is required")
-	}
+	// if config.OBJECT_STORAGE_BUCKET == "" {
+	// 	err = errors.New("OBJECT_STORAGE_BUCKET is required")
+	// }
 
-	if config.SEND_GRID_KEY == "" {
-		err = errors.New("SEND_GRID_KEY is required")
-	}
+	// if config.SEND_GRID_KEY == "" {
+	// 	err = errors.New("SEND_GRID_KEY is required")
+	// }
 
-	return
+	// return
 }
