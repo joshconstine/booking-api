2d8ba34ca42545adbb811b26cfacf760f3080f96
joshua constine <joshua.constine97@gmail.com>
2024-04-13 13:09:27 -0700

    updated photo response

--- models/photo.go
+++ models/photo.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"booking-api/config"
 	"booking-api/data/response"
+	"path"
 
 	"gorm.io/gorm"
 )
@@ -16,8 +18,15 @@ func (p *Photo) TableName() string {
 }
 
 func (p *Photo) MapPhotoToResponse() response.PhotoResponse {
+
+	// load config
+	env, err := config.LoadConfig(".")
+	if err != nil {
+		return response.PhotoResponse{}
+	}
+	urlstring := path.Join("https://", env.OBJECT_STORAGE_URL, p.URL)
 	return response.PhotoResponse{
 		ID:  p.ID,
-		URL: p.URL,
+		URL: urlstring,
 	}
 }
