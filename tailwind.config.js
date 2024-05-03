5827fdaab93461f23101f5ea1f1c32e79b972184
Joshua Constine <joshuaconstine@Joshuas-MacBook-Pro.local>
2024-05-03 07:06:28 -0700

    added tw config for theme colors and added to navbar;

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
     content: [ "./**/*.html", "./**/*.templ", "./**/*.go", ],
    safelist: [],
+   theme: {
+       extend: {
+           colors: {
+              "branded": {
+                //   "100": "#E6F6FF",
+                //   "200": "#BFE8FF",
+                //   "300": "#99DAFF",
+                  "400": "#B2C2C1",
+                //   "500": "#00A6FF",
+                //   "600": "#0096E6",
+                //   "700": "#00647F",
+                  "800": "#586A5D",
+                //   "900": "#00333A",
+              },
+           }
+       }
+   },
    plugins: [require("daisyui")],
    daisyui: {
        themes: [ "light"]
